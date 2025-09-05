13437254756ad430032acd71495eb32dd46048e8
Your Name <you@example.com>
2025-09-05 18:24:36 -0500

    bajamos node a 18

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
 import { Uno } from './components/Uno';
 import { Dos } from './components/Dos';
